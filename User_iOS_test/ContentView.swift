b39ae935323d9c22f38122e20beb1c0a099a4602
Anirudha Mahale <anirudhamahale.com>
2024-01-23 14:37:54 +0530

    Initial Commit

---
+++ User_iOS_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  User_iOS_test
+//
+//  Created by Anirudha Mahale on 23/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ User_iOS_test/User_iOS_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  User_iOS_testApp.swift
+//  User_iOS_test
+//
+//  Created by Anirudha Mahale on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct User_iOS_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
